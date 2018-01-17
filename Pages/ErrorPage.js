a4bc081e96b6822948aaa85bdb721c7f51adb411
tinypt <PjungSs@hotmail.co.th>
2018-01-17 23:24:16 +0700

    ADD Page/reportProblem page for who isn't wipper that will can't go to add report

---
+++ Pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react'
+import { Button ,Text ,View , StyleSheet} from 'react-native'
+
+export default class PagrOfError extends React.Component{
+    rander(){
+        return{
+            <View>
+                <Text>You don't have permission to access</Text>
+            </View>
+        }
+    }
+}
