dbf5046443bb83a0394baad8dafa05e6d5dbe69c
boat46450 <boat46450@hotmail.com>
2018-01-18 17:22:05 +0700

    EDIT feature/reportProblem add filter reducer

--- utils/Reducers.js
+++ utils/Reducers.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import InitReducer from '../ducks/InitState'
 import ReportReducer from '../ducks/ReportProblem'
 import NewProblemReducer from '../ducks/NewProblem'
+import FilerReducer from '../ducks/Filter'
 
 const combine = combineReducers({
     InitReducer,
     ReportReducer,
-    NewProblemReducer
+    NewProblemReducer,
+    FilerReducer
 })
 
 export default combine
