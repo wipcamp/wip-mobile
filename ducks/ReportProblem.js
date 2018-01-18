ea5022823f3a5fca5e5a099ac9450307ab2f9544
boat46450 <boat46450@hotmail.com>
2018-01-18 12:10:01 +0700

    EDIT feature/reportProblem add solve problem

--- ducks/ReportProblem.js
+++ ducks/ReportProblem.js
@@ -1,16 +1,23 @@
 import { initState } from './InitState'
 
 const ADD_PROBLEM = "ADD_PROBLEM"
+const SOLVE_PROBLEM = "SOLVE_PROBLEM"
 
 export default function reducer (state = initState, action) {
     switch (action.type) {
         case ADD_PROBLEM : 
             return {
-                problem: [
+                problem : [
                     ...state.problem,
                     action.problem
                 ]
             }
+        case SOLVE_PROBLEM :
+            return state.problem.map(problem => 
+                (problem.id == action.id)
+                    ? {...problem, isSolve: true}
+                    : problem
+            )
         default : 
             return state
     }
@@ -19,4 +26,9 @@ export default function reducer (state = initState, action) {
 export const addProblem = problem => ({
     type : ADD_PROBLEM,
     problem
+})
+
+export const solveProblem = id => ({
+    type : SOLVE_PROBLEM,
+    id
 })
