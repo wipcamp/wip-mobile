0e9b1aaeba0eabe4e890ff5abf1e475e925c78fb
boat46450 <boat46450@hotmail.com>
2018-01-19 22:45:49 +0700

    ADD feature/reportProblem catagory component

---
+++ components/CatagoryComponent.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Dropdown } from 'react-native-material-dropdown'
+
+import { setNewProblemType } from '../ducks/NewProblem'
+import Styles from '../styles/reportProblemStyle'
+
+const mapStateToProps = state => {
+    return {
+        catagory : state.NewProblemReducer.newProblem.problemType
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setCatagory : bindActionCreators(setNewProblemType, dispatch)
+    }
+}
+
+class Catagory extends Component {
+    componentWillMount() {
+
+    }
+
+    render() {
+        let mockData = [
+            { value: 1, label: "Banana" },
+            { value: 2, label: "Mango" },
+            { value: 3, label: "Pear" },
+            { value: 4, label: "Apple" },
+            { value: 5, label: "Pineapple" }
+        ]
+
+        return (
+            <View style={Styles.bgWhite}>
+                <Dropdown
+                    label="Catagory"
+                    data={mockData}
+                    onChangeText={(value, index, data) => {
+                        this.props.setCatagory(value)
+                    }}
+                />
+            </View>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Catagory)
