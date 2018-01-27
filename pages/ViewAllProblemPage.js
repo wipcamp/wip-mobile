7c04da5261e2d24b55b063cfe3d4cbe2307c4b10
DESKTOP-UHL3ELL\Chonticha Sae-jiw <puifaisme@gmail.com>
2018-01-27 15:42:35 +0700

    EDIT feature/viewProblem decorate ViewAllProblem page

--- pages/ViewAllProblemPage.js
+++ pages/ViewAllProblemPage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
+import Catagory from '../components/CatagoryComponent'
+import Styles from '../styles/ViewAProblemStyle'
+import ProblemStyles from '../styles/ViewAllProblemStyle'
 
 export default class ViewAllProblemPage extends Component {
   render() {
     return (
       <View>
-        <Text>  </Text>
+        <Text style={[ Styles.boxGray , Styles.topicGray, Styles.border]}> </Text>
+        <View>
+          <Catagory/>
+        </View>
+        <Text style={[ Styles.boxGray , Styles.topicGray, Styles.border]}> </Text>
+        <View>
+          <Text style={Styles.boxWhite}>Topic Name</Text>
+          <Text>Sunday</Text>
+          <Text>Catagory: Catagory Name</Text>
+          
+        </View>
       </View>
     );
   }
--- styles/ViewAllProblemStyle.js
+++ styles/ViewAllProblemStyle.js
@@ -1,8 +1,7 @@
 import { StyleSheet, Dimensions } from 'react-native'
 
 const style = StyleSheet.create({
-    
-      
+         
 })
 
 export default style
