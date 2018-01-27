f67a11d7442e4788392bb2302ceb0bc454e90aa3
DESKTOP-UHL3ELL\Chonticha Sae-jiw <puifaisme@gmail.com>
2018-01-28 00:09:41 +0700

    EDIT feature/ViewAllProblem add styles and mock

--- pages/ViewAllProblemPage.js
+++ pages/ViewAllProblemPage.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
+import Catagory from '../components/CatagoryComponent';
+import Styles from '../styles/ViewAProblemStyle';
+import AllPagestyle from '../styles/ViewAllProblemStyle';
 
-export default class ViewAllProblemPage extends Component {
+  class AllProblem extends Component {
+    static navigationOptions = {
+        title: 'Problem',
+        headerRight: <Text>+</Text>
+    }
   render() {
     return (
       <View>
-        <Text>  </Text>
+        <View>
+          <Text style={[Styles.boxGray , Styles.topicGray, Styles.border]}></Text>
+        </View>
+        <View>
+          <Catagory/>
+        </View>
+          <Text style={[Styles.boxGray , Styles.topicGray, Styles.border]}></Text>
+        <View style={[ Styles.boxWhite, Styles.topicWhite, Styles.border]}>
+          <View styles={AllPagestyle.flexbox}>
+            <Text>Topic Name</Text>
+            <Text styles={AllPagestyle.textright}>Sunday</Text>
+          </View>
+          <Text>Catagory : Catagory Name</Text>
+        </View>
+        <View style={[ Styles.boxWhite, Styles.topicWhite, Styles.border, AllPagestyle.flexbox]}>
+          <View styles={AllPagestyle.flexbox}>
+            <Text>Topic Name</Text>
+            <Text styles={AllPagestyle.textright}>Munday</Text>
+          </View>
+          <Text>Catagory : Catagory Name</Text>
+        </View>
+        <View style={[ Styles.boxWhite, Styles.topicWhite, Styles.border, AllPagestyle.flexbox]}>
+          <View styles={AllPagestyle.flexbox}>
+            <Text>Topic Name</Text>
+            <Text styles={AllPagestyle.textright}>Munday</Text>
+          </View>
+          <Text>Catagory : Catagory Name</Text>
+        </View>
       </View>
     );
   }
 }
+export default AllProblem
--- styles/ViewAllProblemStyle.js
+++ styles/ViewAllProblemStyle.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Dimensions } from 'react-native'
 
 const style = StyleSheet.create({
-    
+    flexbox: {
+        display: flex,
+        flexWrap: nowrap
+    },
+    textright: {
+        alignContent: 'end',
+    }
       
 })
 
