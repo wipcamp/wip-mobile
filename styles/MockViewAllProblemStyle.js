07df0e312638589305569c76f454a56bbf9d64e9
boat46450 <boat46450@hotmail.com>
2018-01-29 21:59:03 +0700

    EDIT feature/viewProblem rename to mock

--- styles/MockViewAllProblemStyle.js
+++ styles/MockViewAllProblemStyle.js
@@ -2,11 +2,11 @@ import { StyleSheet, Dimensions } from 'react-native'
 
 const style = StyleSheet.create({
     flexbox: {
-        display: flex,
-        flexWrap: nowrap
+        // display: flex,
+        // flexWrap: nowrap
     },
     textright: {
-        alignContent: 'end',
+        // alignContent: 'end',
     },
     titleStyle: {
         fontSize: 18,
