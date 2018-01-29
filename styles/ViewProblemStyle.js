27b18b8fec960431ee39444d92bad01bb063dac5
boat46450 <boat46450@hotmail.com>
2018-01-30 00:44:36 +0700

    ADD feature/viewProblem style for view problem

---
+++ styles/ViewProblemStyle.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native'
+
+const style = StyleSheet.create({
+    topic: {
+        fontSize: 16
+    },
+    description: {
+        fontSize: 14
+    },
+    date: {
+        fontSize: 10
+    },
+    column: {
+        flexDirection: 'column'
+    },
+    row: {
+        flexDirection: 'row'
+    },
+    flex05: {
+        flex: 0.5
+    },
+    flex1: {
+        flex: 1
+    },
+    flex4: {
+        flex: 4
+    },
+    itemCenter: {
+        alignItems: 'center'
+    },
+    disable: {
+        backgroundColor: '#E4E4E4'
+    }
+})
+
+export default style
