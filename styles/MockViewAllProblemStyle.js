de6106ff34e241bc2256a1699a6df7fe20505d6e
DESKTOP-UHL3ELL\Chonticha Sae-jiw <puifaisme@gmail.com>
2018-01-31 00:53:48 +0700

    EDIT feature/viewProblem decorate in MockViewAllProblem for page and style.

--- styles/MockViewAllProblemStyle.js
+++ styles/MockViewAllProblemStyle.js
@@ -1,17 +1,26 @@
 import { StyleSheet, Dimensions } from 'react-native'
 
 const style = StyleSheet.create({
-    flexbox: {
-        // display: flex,
-        // flexWrap: nowrap
+    flexrow: {
+      flex: 1, 
+      flexDirection: 'row',
+      width: 50,
+      hight: 50
     },
-    textright: {
-        // alignContent: 'end',
+    boxwhite: {
+      backgroundColor: '#ffffff',
     },
-    titleStyle: {
-        fontSize: 18,
-        paddingLeft: 15
-    } 
+    boxgray: {
+      flex: 1,
+      flexDirection: 'row',
+      width: 50,
+      height: 25
+    },
+    bg: {
+        backgroundColor: '#ECEFF1',
+        flex:1
+    }
 })
+
 
 export default style
