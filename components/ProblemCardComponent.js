6ecab31081d9ab19f2dbb332170dad1f04337fa5
boat46450 <boat46450@hotmail.com>
2018-01-30 22:18:28 +0700

    EDIT feature/viewProblem change format data

--- components/ProblemCardComponent.js
+++ components/ProblemCardComponent.js
@@ -16,24 +16,45 @@ class ProblemCard extends Component {
                 onPress={() => this.props.navigation.navigate('AProblem', {id: this.props.data.id})}
             >
                 <View
-                    style={[Styles.row, (this.props.data.is_solve || this.props.data.not_solve)
-                                            ? Styles.disable
-                                            : ReportStyle.bgWhite]}
+                    style={[
+                        Styles.row,
+                        Styles.border,
+                        (this.props.data.is_solve || this.props.data.not_solve)
+                            ? Styles.disable
+                            : ReportStyle.bgWhite
+                    ]}
                 >
                     <View style={[Styles.flex05, Styles.itemCenter]}>
-                        <Text>[]</Text>
+                        {
+                            this.props.data.is_solve || this.props.data.not_solve
+                            ? <Text></Text>
+                            : <Text>[]</Text>
+                        }
                     </View>
                     <View style={[Styles.column, Styles.flex4]}>
                         <Text style={Styles.topic}>
-                            {this.props.data.topic.substr(0, 30) + '...'}
+                            {
+                                this.props.data.topic.lenght <= 25
+                                ? this.props.data.topic.substr(0, 25)
+                                : this.props.data.topic.substr(0, 25) + '...'
+                            }
                         </Text>
                         <Text style={Styles.description}>
-                            {this.props.data.description.substr(0, 50) + '...'}
+                            {
+                                this.props.data.topic.description <= 50
+                                ? this.props.data.description.substr(0, 50)
+                                : this.props.data.description.substr(0, 50) + '...'
+                            }
                         </Text>
                     </View>
                     <View style={[Styles.flex1, Styles.itemCenter]}>
                         <Text style={Styles.date}>
-                            {this.props.data.created_at.split(" ")[0]}
+                            {
+                                this.props.data.created_at
+                                    .split(" ")[0]
+                                    .split("-")
+                                    .join("/")
+                            }
                         </Text>
                     </View>
                 </View>
