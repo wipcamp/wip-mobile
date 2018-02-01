1e75b61cb289989f2e4f09210236e1d9c643d04d
boat46450 <boat46450@hotmail.com>
2018-02-01 23:31:33 +0700

    EDIT feature/login change flow

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { View, Text, Button, Image, StyleSheet } from 'react-native'
-import { AuthSession } from 'expo'
+import { Facebook } from 'expo'
 
+import env from '../config'
 import Styles from '../styles/LoginStyle'
 import WipLogo from '../src/images/Logo_WIPCamp.png'
-
-const FB_APP_ID = '911632835680936';
 
 class Login extends Component {
     static navigationOptions = {
@@ -46,36 +45,18 @@ class Login extends Component {
     }
 
     _handlePressAsync = async () => {
-        let redirectUrl = AuthSession.getRedirectUrl()
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync(`${env.FB_APP_ID}`, {
+            permissions: ['public_profile']
+          })
 
-        // You need to add this url to your authorized redirect urls on your Facebook app
-        console.log({ redirectUrl })
-
-        // NOTICE: Please do not actually request the token on the client (see:
-        // response_type=token in the authUrl), it is not secure. Request a code
-        // instead, and use this flow:
-        // https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow/#confirm
-        // The code here is simplified for the sake of demonstration. If you are
-        // just prototyping then you don't need to concern yourself with this and
-        // can copy this example, but be aware that this is not safe in production.
-
-        let result = await AuthSession.startAsync({
-            authUrl:
-            `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
-            `&client_id=${FB_APP_ID}` +
-            `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
-        })
-
-        if (result.type !== 'success') {
+        if (type !== 'success') {
             alert('Uh oh, something went wrong')
             return
         }
-
-        let accessToken = result.params.access_token;
-        let userInfoResponse = await fetch(
-            `https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,picture.type(large)`
+        const response = await fetch(
+            `https://graph.facebook.com/me?access_token=${token}&fields=id,name,picture.type(large)`
         )
-        const userInfo = await userInfoResponse.json()
+        const userInfo = await response.json()
         this.setState({ userInfo })
     }
 }
