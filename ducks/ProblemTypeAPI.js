8340fab2477b43ec5c96fda761d8f14b0a7fdafa
boat46450 <boat46450@hotmail.com>
2018-02-04 20:34:53 +0700

    ADD feature/test problem type api action

---
+++ ducks/ProblemTypeAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+import { addCategory, resetCategory } from './CategoryProblem'
+
+import env from '../config'
+
+export function getAll() {
+    return async dispatch => {
+        dispatch(resetCategory())
+        let api = await axios.get(`${env.API_URL}/problemtypes/`)
+        let datas = api.data
+        datas.map(data => {
+            let category = {
+                value: data.id,
+                label: data.name
+            }
+            dispatch(addCategory(category))
+        })
+    }
+}
