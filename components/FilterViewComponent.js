9c0e9670cf51708ce44c01c15d87a0f66fe7c279
boat46450 <boat46450@hotmail.com>
2018-02-06 22:38:41 +0700

    EDIT feature/test remove connect & change flow of component will mount

--- components/FilterViewComponent.js
+++ components/FilterViewComponent.js
@@ -1,41 +1,14 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 import { Dropdown } from 'react-native-material-dropdown'
-import axios from 'axios'
 
-import env from '../config'
 import { setFilter, addFilter } from '../ducks/Filter'
 import ReportStyles from '../styles/reportProblemStyle'
 
-const mapStateToProps = state => {
-    return {
-        filterView : state.FilterReducer.filterView,
-        filter : state.FilterReducer.filter
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        addFilter : bindActionCreators(addFilter, dispatch),
-        setFilter : bindActionCreators(setFilter, dispatch)
-    }
-}
-
 class FilterView extends Component {
 
-    async componentWillMount() {
-        let api = await axios.get(`${env.API_URL}/problemtypes/`)
-        let datas = api.data
-        datas.map(data => {
-            let filter = {
-                value: data.id,
-                label: data.name
-            }
-            this.props.addFilter(filter)
-        })
-        this.props.setFilter(0)
+    componentWillMount() {
+        this.props.problemTypeGetAllForFilter()
     }
 
     render() {
@@ -57,4 +30,4 @@ class FilterView extends Component {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FilterView)
+export default FilterView
