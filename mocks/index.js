a64dd4d0fa4772be0a90246f820e0bda87545e76
boat46450 <boat46450@hotmail.com>
2018-02-08 00:01:51 +0700

    EDIT feature/test remove mock flatlist

--- mocks/index.js
+++ mocks/index.js
@@ -8,13 +8,6 @@ module.exports = {
         ScrollView.prototype.render = RealScrollView.prototype.render
         return ScrollView
       })
-  
-      jest.mock('FlatList', () => {
-        const RealListView = require.requireActual('FlatList')
-        const ListView = mockComponent('FlatList')
-        ListView.prototype.render = RealListView.prototype.render
-        return ListView
-      })
 
       jest.mock('Dimensions', () => {
         const Dimensions = {
