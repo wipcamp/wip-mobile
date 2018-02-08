f08932a564a794873a58b86b888b5ab9ca5389e1
boat46450 <boat46450@hotmail.com>
2018-02-08 20:51:23 +0700

    EDIT feature/test add get & put function

--- ducks/ProblemAPI.js
+++ ducks/ProblemAPI.js
@@ -2,8 +2,20 @@ import axios from 'axios'
 
 import env from '../config'
 
+export function get(id) {
+    return async dispatch => (
+        await axios.get(`${env.API_URL}/problems/${id}`)
+    )
+}
+
 export function post(data) {
     return async dispatch => (
         await axios.post(`${env.API_URL}/problems/`, data)
+    )
+}
+
+export function put(id, data) {
+    return async dispatch => (
+        await axios.put(`${env.API_URL}/problems/${id}`, data)
     )
 }
