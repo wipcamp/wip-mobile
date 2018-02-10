8422862710e11b55b68f42e94c6a4198eded323f
boat46450 <boat46450@hotmail.com>
2018-02-11 00:57:18 +0700

    EDIT fix/api use api pattern and prepare for error from token

--- ducks/ProblemAPI.js
+++ ducks/ProblemAPI.js
@@ -1,21 +1,31 @@
-import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-import env from '../config'
+import api from '../utils/api'
+import { getToken } from '../utils/apiAuth'
 
 export function get(id) {
-    return async dispatch => (
-        await axios.get(`${env.API_URL}/problems/${id}`)
-    )
+    return async dispatch => {
+        let api = await api.get(`/problems/${id}`, {Authorization: `Bearer ${getToken()}`})
+        if(api.data.error) {
+        }
+        return api
+    }
 }
 
 export function post(data) {
-    return async dispatch => (
-        await axios.post(`${env.API_URL}/problems/`, data)
-    )
+    return async dispatch => {
+        let api = await api.post('/problems/', data, {Authorization: `Bearer ${getToken()}`})
+        if(api.data.error) {
+        }
+        return api
+    }
 }
 
 export function put(id, data) {
-    return async dispatch => (
-        await axios.put(`${env.API_URL}/problems/${id}`, data)
-    )
+    return async dispatch => {
+        let api = await api.put(`/problems/${id}`, data, {Authorization: `Bearer ${getToken()}`})
+        if(api.data.error) {
+        }
+        return api
+    }
 }
