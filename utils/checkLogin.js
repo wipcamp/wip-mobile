8b19d40fa14e64e33e286165435e7f6c88d1e28a
boat46450 <boat46450@hotmail.com>
2018-02-14 21:31:39 +0700

    DELETE feature/login don't use

--- utils/checkLogin.js
+++
@@ -1,9 +0,0 @@
-import { AsyncStorage } from 'react-native'
-
-export default async function checkLogin() {
-    const user = await AsyncStorage.getItem('user')
-    if(user == null) {
-        return false
-    }
-    return true
-}
