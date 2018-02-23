428319837e277ee5e3c59602342db2ddfed31022
boat46450 <boat46450@hotmail.com>
2018-02-23 22:27:53 +0700

    ADD feature/timetable timetable row page

---
+++ pages/TimetableRowPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+class TimetableRole extends Component {
+
+    static navigationOptions = {
+        title: 'Time Schedule'
+    }
+
+    render() {
+        return (
+            <View>
+            </View>
+        )
+    }
+}
+
+export default TimetableRole
