4825ddb58da21e6c0086e9dfac2da88d6edf2b93
boat46450 <boat46450@hotmail.com>
2018-02-23 23:49:29 +0700

    EDIT feature/timetable add role card

--- pages/TimetableRowPage.js
+++ pages/TimetableRowPage.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, AsyncStorage } from 'react-native'
+
+import { getByName as roleGetByName } from '../utils/apiRole'
+import { getByName as roleTeamGetByName } from '../utils/apiRoleTeam'
+
+import RoleCard from '../components/RoleCardComponent'
+import Styles from '../styles/TimetableStyle'
+import ReportStyles from '../styles/reportProblemStyle'
 
 class TimetableRole extends Component {
 
@@ -9,7 +16,64 @@ class TimetableRole extends Component {
 
     render() {
         return (
-            <View>
+            <View style={ReportStyles.bg}>
+                <RoleCard
+                    title="P'ITIM"
+                    onPressFunction={async () => {
+                        let id = await roleTeamGetByName('ITIM')
+                        this.props.navigation.navigate('Timetable', {id: id})
+                    }}
+                />
+                <RoleCard
+                    title="P'VICHAKARN"
+                    onPressFunction={async () => {
+                        let id = await roleTeamGetByName('VICHAKARN')
+                        this.props.navigation.navigate('Timetable', {id: id})
+                    }}
+                />
+                <RoleCard
+                    title="P'SWATDIKARN"
+                    onPressFunction={async () => {
+                        let id = await roleTeamGetByName('SWATDIKARN')
+                        this.props.navigation.navigate('Timetable', {id: id})
+                    }}
+                />
+                <RoleCard
+                    title="P'LOCATION"
+                    onPressFunction={async () => {
+                        let id = await roleTeamGetByName('LOCATION')
+                        this.props.navigation.navigate('Timetable', {id: id})
+                    }}
+                />
+                <RoleCard
+                    title="P'MC"
+                    onPressFunction={async () => {
+                        let id = await roleTeamGetByName('MC')
+                        this.props.navigation.navigate('Timetable', {id: id})
+                    }}
+                />
+                <RoleCard
+                    title="P'WIPPER"
+                    onPressFunction={async () => {
+                        let user = await AsyncStorage.getItem('user')
+                        user = JSON.parse(user)
+                        let seniorId = await roleGetByName('camp_staffs_senior')
+                        let senior = false
+                        for (let i = 0; i < user.roles.length; i++) {
+                            if (user.roles[i] == seniorId) {                                
+                                senior = true
+                            }
+                        }
+                        if (senior) {
+                            console.log('senior')
+                            let id = await roleTeamGetByName('WIPPER')
+                            this.props.navigation.navigate('Timetable', {id: id}) 
+                        } 
+                        else {                            
+                            this.props.navigation.navigate('Error')
+                        }
+                    }}
+                />
             </View>
         )
     }
