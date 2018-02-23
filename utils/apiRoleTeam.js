6887f664f5917dd9ae39878998fa985fcb2b57d9
boat46450 <boat46450@hotmail.com>
2018-02-23 18:08:10 +0700

    ADD feature/timetable api role team, user role team, timetable

---
+++ utils/apiRoleTeam.js
@@ -0,0 +1,16 @@
+import Api from './api'
+import { getToken } from './apiAuth'
+
+export async function getByName(name) {
+    let api
+    try {
+        api = await Api.get(`/roleteams/name/${name}`, {Authorization: `Bearer ${await getToken()}`})
+    }
+    catch (error) {
+        if(error.status == 401) {
+            await refresh()
+            api = await Api.get(`/roleteams/name/${name}`, {Authorization: `Bearer ${await getToken()}`})
+        }
+    }
+    return api.data
+}
