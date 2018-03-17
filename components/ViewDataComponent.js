cf58dc04d09f8808130e073e147c1e020adfe68d
boat46450 <boat46450@hotmail.com>
2018-03-17 22:40:01 +0700

    ADD refactor/viewproblem component for show data

---
+++ components/ViewDataComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+import Styles from '../styles/ViewProblemStyle'
+import ReportStyle from '../styles/reportProblemStyle'
+
+class ViewData extends Component {
+    render() {
+        return (
+            <View
+                style={[
+                    ReportStyle.bgWhite,
+                    ReportStyle.inputPadding,
+                    Styles.row,
+                    Styles.border
+                ]}
+            >
+                <View style={[Styles.flex1]}>
+                    <Text>{this.props.left}</Text>
+                </View>
+                <View style={[Styles.flex1, Styles.itemRight]}>
+                    <Text>{this.props.right}</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default ViewData
