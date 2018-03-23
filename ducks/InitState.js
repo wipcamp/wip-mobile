e583f55c49fe2c3b8631305cf6a140345de206d6
boat46450 <boat46450@hotmail.com>
2018-03-24 00:32:37 +0700

    EDIT feature/viewProblem add default priority & new priority id

--- ducks/InitState.js
+++ ducks/InitState.js
@@ -5,9 +5,11 @@ export const initState = {
         topic: "",
         problem_type_id: 0,
         description: "",
-        report_id: null
+        report_id: null,
+        priority_id: 0
     }, // for report problem any input
     categoryProblem : [],
+    priority : [],
     filterView : [{
         value: 0,
         label: "ทั้งหมด"
