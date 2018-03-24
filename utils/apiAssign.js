1a29a8415d7f87d78c8c6853ddfaff2e1852b6f7
boat46450 <boat46450@hotmail.com>
2018-03-24 11:50:43 +0700

    EDIT feature/viewProblem add getByRoleTeamId & getByAssignedId

--- utils/apiAssign.js
+++ utils/apiAssign.js
@@ -14,3 +14,31 @@ export async function getByProblemId(id) {
     }
     return api.data
 }
+
+export async function getByRoleTeamId(id) {
+    let api
+    try {
+        api = await Api.get(`/assigns/role_team_id/${id}`, {Authorization: `Bearer ${await getToken()}`})    
+    }
+    catch (error) {
+        if(error.status == 401) {
+            await refresh()
+            api = await Api.get(`/assigns/role_team_id/${id}`, {Authorization: `Bearer ${await getToken()}`})
+        }        
+    }
+    return api.data
+}
+
+export async function getByAssignedId(id) {
+    let api
+    try {
+        api = await Api.get(`/assigns/assigned_id/${id}`, {Authorization: `Bearer ${await getToken()}`})    
+    }
+    catch (error) {
+        if(error.status == 401) {
+            await refresh()
+            api = await Api.get(`/assigns/assigned_id/${id}`, {Authorization: `Bearer ${await getToken()}`})
+        }        
+    }
+    return api.data
+}
