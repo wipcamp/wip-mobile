26c3dc6be7874111d7137a12251d8738900f7ef5
boat46450 <boat46450@hotmail.com>
2018-03-25 00:05:02 +0700

    EDIT feature/timetable add top size

--- components/EventCardComponent.js
+++ components/EventCardComponent.js
@@ -24,12 +24,13 @@ class EventCard extends Component {
     }
 
     _renderHeight() {
+        const height = Dimensions.get('screen').height
         let start = this.props.data.start_on.split(' ')[1].split(':')
         let end = this.props.data.finish_on.split(' ')[1].split(':')
         let diffHour = parseInt(end[0]) - parseInt(start[0])
         let diffMinute = parseInt(end[1]) - parseInt(start[1])
         let minute = this.props.half ? 30 : 60
-        let spacePerN = Dimensions.get('screen').height * 0.12
+        let spacePerN = height * 0.12
 
         let duration = ((diffHour * 60) + diffMinute) * (spacePerN / minute)
 
@@ -46,9 +47,9 @@ class EventCard extends Component {
         let start = this.props.data.start_on.split(' ')[1].split(':')
         start = (parseInt(start[0]) * 60) + parseInt(start[1])
         let minute = this.props.half ? 30 : 60
-        let spacePerN = Dimensions.get('screen').height * 0.12
+        let spacePerN = height * 0.12
         
-        let top = 9 + (start * (spacePerN / minute))
+        let top = 19 + (start * (spacePerN / minute))
 
         return top
     }
