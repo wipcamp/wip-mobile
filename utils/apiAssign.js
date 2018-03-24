7c73d437fec3d2bf5da3566e847d0bc30f9706a6
boat46450 <boat46450@hotmail.com>
2018-03-24 09:42:01 +0700

    ADD feature/viewProblem api assign with get by problem id

---
+++ utils/apiAssign.js
@@ -0,0 +1,16 @@
+import Api from './api'
+import { refresh, getToken } from './apiAuth'
+
+export async function getByProblemId(id) {
+    let api
+    try {
+        api = await Api.get(`/assigns/problem_id/${id}`, {Authorization: `Bearer ${await getToken()}`})    
+    }
+    catch (error) {
+        if(error.status == 401) {
+            await refresh()
+            api = await Api.get(`/assigns/problem_id/${id}`, {Authorization: `Bearer ${await getToken()}`})
+        }        
+    }
+    return api.data
+}
