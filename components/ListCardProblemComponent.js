79930d1a4f36950eb936515e5be3a41c289585d5
boat46450 <boat46450@hotmail.com>
2018-03-24 12:18:14 +0700

    EDIT feature/viewProblem not show solved problem

--- components/ListCardProblemComponent.js
+++ components/ListCardProblemComponent.js
@@ -9,9 +9,13 @@ class ListCardProblem extends Component {
             <FlatList
                 data={this.props.problem}
                 keyExtractor={(item) => item.id}
-                renderItem={({item}) => (
-                    <ProblemCard key={item.id} data={item} navigation={this.props.navigation} />
-                )}
+                renderItem={({item}) => {
+                    if(!(item.is_solve || item.not_solve)) {
+                        return (
+                            <ProblemCard key={item.id} data={item} navigation={this.props.navigation} />
+                        )
+                    }
+                }}
             />
         )
     }
