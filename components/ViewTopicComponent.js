a35d44f4b89b000cf17b2f9cf80372c65d739d70
boat46450 <boat46450@hotmail.com>
2018-03-24 21:29:59 +0700

    ADD feature/timetable recover from last commit

---
+++ components/ViewTopicComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { TextField } from 'react-native-material-textfield'
+
+import Styles from '../styles/ViewProblemStyle'
+import ReportStyles from '../styles/reportProblemStyle'
+
+class ViewTopic extends Component {
+    render() {
+        return (
+            <View style={[ReportStyles.bgWhite, ReportStyles.spacesTop]}>
+                <TextField
+                    label ='Topic'
+                    value={this.props.data.topic}
+                    disabled={false}
+                    editable={false}
+                    multiline={true}
+                    labelFontSize={20}
+                    labelPadding={7}                    
+                    labelHeight={20}
+                    inputContainerPadding={5}
+                    fontSize={18}
+                    containerStyle={[
+                        ReportStyles.inputPadding,
+                        Styles.bgLabel,
+                    ]}
+                    labelTextStyle={[
+                        Styles.textInTopicLabel,
+                        Styles.marginTop10,
+                    ]}
+                />
+            </View>
+        )
+    }
+}
+
+export default ViewTopic 
