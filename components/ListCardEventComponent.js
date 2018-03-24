0fdb32ab3a7764360a89aac1bc450ba61f9919c0
boat46450 <boat46450@hotmail.com>
2018-03-25 00:05:55 +0700

    EDIT feature/timetable filter role team

--- components/ListCardEventComponent.js
+++ components/ListCardEventComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { AsyncStorage, View } from 'react-native'
 
 import EventCard from './EventCardComponent'
 
@@ -7,6 +7,21 @@ import Styles from '../styles/TimetableStyle'
 import ViewStyles from '../styles/ViewProblemStyle'
 
 class ListCardEvent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            roleteams: []
+        }
+    }
+
+    async componentWillMount() {
+        let user = await AsyncStorage.getItem('user')
+        user = JSON.parse(user)
+        this.setState({
+            roleteams: user.roleteams
+        })
+    }
+
     render() {
         return (
             <View style={[Styles.eventContainer]}>
@@ -16,33 +31,10 @@ class ListCardEvent extends Component {
     }
 
     _renderCardEvent() {
-        datas = this.props.timetable.filter(data => this.props.roleId == data.role_team_id)
-        return datas.map(data => {
-            if(this.checkdate(this.props.date, data.start_on)) {
-                return <EventCard key={data.id} data={data} half={this.props.half} />
-            }
-        })
-    }
-
-    checkdate(stateDate, timestamp) {
-        let date = stateDate.split('-')
-
-        switch (date[1]) {
-            case "May":
-                date[1] = 5
-                break;
-            case "Jun":
-                date[1] = 6
-                break;
-            default:
-                break;
-        }
-
-        timestamp = timestamp.split(' ')[0].split('-')
-        let result = (parseInt(date[0]) == parseInt(timestamp[2])) && (date[1] == parseInt(timestamp[1]))
-            ? true
-            : false
-        return result
+        let datas = this.props.timetable.filter(data => this.state.roleteams.indexOf(data.role_team_id) >= 0)
+        return datas.map(data => (
+            <EventCard key={data.id} data={data}/>
+        ))
     }
 }
 
