9bcd2eebe10e8a36d712838ecd72982cb8571074
boat46450 <boat46450@hotmail.com>
2018-03-24 09:53:46 +0700

    EDIT feature/viewProblem add priority state & priority action to props

--- pages/ConnectViewAProblemPage.js
+++ pages/ConnectViewAProblemPage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import { updateProblem } from '../ducks/ReportProblem'
 import { getAllForCategory } from '../ducks/ProblemTypeAPI'
+import { addPriority } from '../ducks/Priority'
 
 import ViewAProblem from './ViewAProblemPage'
 
@@ -10,13 +11,15 @@ const mapStateToProps = state => {
     return {
         problem : state.ReportReducer.problem,
         categoryProblem : state.CategoryReducer.categoryProblem,
+        priority : state.PriorityReducer.priority
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         updateProblem : bindActionCreators(updateProblem, dispatch),
-        problemTypeGetAllForCategory : bindActionCreators(getAllForCategory, dispatch)
+        problemTypeGetAllForCategory : bindActionCreators(getAllForCategory, dispatch),
+        addPriority : bindActionCreators(addPriority, dispatch)
     }
 }
 
