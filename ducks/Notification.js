69b28a5e3e62133a73be1ba77362d34d17eca11f
boat46450 <boat46450@hotmail.com>
2018-03-28 20:50:51 +0700

    ADD feature/notification notification duck with add, reverse, reset

---
+++ ducks/Notification.js
@@ -0,0 +1,40 @@
+import { initState } from './InitState'
+
+const ADD_NOTIFICATION = "ADD_NOTIFICATION"
+const REVERSE_NOTIFICATION = "REVERSE_NOTIFICATION"
+const RESET_NOTIFICATION = "RESET_NOTIFICATION"
+
+export default function reducer (state = initState, action) {
+    switch (action.type) {
+        case ADD_NOTIFICATION:
+            return {
+                notification: [
+                    ...state.notification,
+                    action.notification
+                ]
+            }
+        case REVERSE_NOTIFICATION:
+            return {
+                notification: state.notification.reverse()
+            }
+        case RESET_NOTIFICATION:
+            return {
+                notification: []
+            }
+        default:
+            return state
+    }
+}
+
+export const addNotification = notification => ({
+    type: ADD_NOTIFICATION,
+    notification
+})
+
+export const reverseNotification = () => ({
+    type: REVERSE_NOTIFICATION
+})
+
+export const resetNotification = () => ({
+    type: RESET_NOTIFICATION
+})
