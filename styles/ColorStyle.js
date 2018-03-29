77292ec0e9375ad27fefab11a1086fa2e9317d81
boat46450 <boat46450@hotmail.com>
2018-03-30 00:27:13 +0700

    ADD refactor/ui style for color

---
+++ styles/ColorStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    bgGrey: {
+        backgroundColor: '#EFEFEF'
+    },
+    bgWhite: {
+        backgroundColor: '#FFF'
+    },
+    textWhite: {
+        color: '#FFF'
+    }
+})
+
+export default styles
