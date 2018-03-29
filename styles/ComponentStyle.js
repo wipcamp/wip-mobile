2f6f4c2749260cdd9d2758628ef70fa628772b2a
boat46450 <boat46450@hotmail.com>
2018-03-30 00:27:45 +0700

    ADD refactor/ui style for component

---
+++ styles/ComponentStyle.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    facebookButton: {
+        backgroundColor: '#4867AD',
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 60,
+        paddingRight: 60,
+        borderRadius: 20,
+    },
+    inputField: {
+        flexWrap: 'wrap'
+    },
+    addButton: {
+        color: "#1b94f1",
+        fontSize: 17,
+        marginRight: 10
+    }
+})
+
+export default styles
