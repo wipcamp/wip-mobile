2f005054430f619d8369b490873d763f082b1e8f
boat46450 <boat46450@hotmail.com>
2018-03-31 00:04:31 +0700

    EDIT refactor/ui role team get by id

--- utils/apiRoleTeam.js
+++ utils/apiRoleTeam.js
@@ -1,6 +1,20 @@
 import Api from './api'
 import { refresh, getToken } from './apiAuth'
 
+export async function get(id) {
+    let api
+    try {
+        api = await Api.get(`/roleteams/${id}`, {Authorization: `Bearer ${await getToken()}`})
+    }
+    catch (error) {
+        if(error.status == 401) {
+            await refresh()
+            api = await Api.get(`/roleteams/${id}`, {Authorization: `Bearer ${await getToken()}`})
+        }
+    }
+    return api.data
+}
+
 export async function getByName(name) {
     let api
     try {
