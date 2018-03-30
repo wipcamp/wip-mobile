a7f1ea44a7605af136f3f38199fcaf4d35141c04
boat46450 <boat46450@hotmail.com>
2018-03-31 00:03:19 +0700

    ADD refactor/ui tab navigator

---
+++ utils/FeatureNavigation.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { TabNavigator, TabBarBottom } from 'react-navigation'
+import { Icon } from 'native-base'
+
+import MainPage from '../pages/NetMainPage'
+import TimetablePage from '../pages/NetTimetablePage'
+import AllProblemPage from '../pages/NetViewAllProblemPage'
+import ReportProblemPage from '../pages/NetReportProblemPage'
+
+import AddButton from '../components/ConnectAddButtonComponent'
+
+const d = new Date(2018, 5, 1)
+let year = d.getFullYear()
+let month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1
+let day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate()
+let date = `${year}-${month}-${day}`
+
+const FeatureNavigation = TabNavigator(
+    {
+        Main: {
+            screen: MainPage,
+            navigationOptions: {
+                headerRight: null,
+                tabBarLabel: 'Home',
+                tabBarIcon: () => <Icon ios='ios-home' android="md-home" style={{color: '#FFF'}} />
+            }
+        },
+        AllProblem: {
+            screen: AllProblemPage,
+            navigationOptions: {
+                title: 'Problem',
+                tabBarLabel: 'Problem',
+                tabBarIcon: () => <Icon ios='ios-search' android="md-search" style={{color: '#FFF'}} />
+            }
+        },
+        Timetable: {
+            screen: TimetablePage,
+            navigationOptions: {
+                title: date,
+                tabBarLabel: 'Timtetable',
+                tabBarIcon: () => <Icon ios='ios-calendar' android="md-calendar" style={{color: '#FFF'}} />
+            }
+        },
+        ReportProblem: {
+            screen: ReportProblemPage,
+            navigationOptions: ({ navigation }) => ({
+                title: 'ReportProblem',
+                headerRight: <AddButton navigation={navigation} />,
+                tabBarLabel: 'Report',
+                tabBarIcon: () => <Icon ios='ios-create' android="md-create" style={{color: '#FFF'}} />                
+            })
+        },
+        Profile: {
+            screen: ReportProblemPage,
+            navigationOptions: {
+                title: 'Profile',
+                tabBarLabel: 'Profile',
+                tabBarIcon: () => <Icon ios='ios-contact' android="md-contact" style={{color: '#FFF'}} />
+            }
+        }
+    },
+    {
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: 'bottom',
+        swipeEnabled: true,
+        lazy: true,
+        tabBarOptions: {
+            showIcon: true,
+            showLabel: true,
+            activeTintColor: '#FFF',
+            activeBackgroundColor: '#FF8214',
+            inactiveTintColor: '#FFF',
+            inactiveBackgroundColor: '#C8C8C8'
+        }
+    }
+)
+
+export default FeatureNavigation
