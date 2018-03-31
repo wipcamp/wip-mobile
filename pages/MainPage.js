c24c964f964fa61676460ee2c258c366a9adfdfb
boat46450 <boat46450@hotmail.com>
2018-03-31 18:35:56 +0700

    EDIT refactor/ui add navigate to login page

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -19,6 +19,9 @@ class MainPage extends Component {
 
     async componentWillMount() {
         let user = await AsyncStorage.getItem('user')
+        if(!user) {
+            this.props.navigation.navigate('Login')
+        }
         user = JSON.parse(user)
         this.setState({userProfile: user})
         this.props.setReportId(this.state.userProfile.user_id)
@@ -30,27 +33,6 @@ class MainPage extends Component {
     render() {
         return (
             <View style={[ReportStyle.bg, Styles.column]}>
-                <View 
-                    style={[
-                        Styles.row,
-                        Styles.flex01,
-                        Styles.justifyEnd,
-                        Styles.alignCenter,
-                        Styles.padTop20,
-                        Styles.marginRight10
-                    ]}
-                >
-                    <Text style={Styles.marginRight10}>
-                        { this.state.userProfile 
-                            ? this.state.userProfile.nickname
-                            : null
-                        }
-                    </Text>
-                    { this.state.userProfile
-                        ? this.__renderProfileImg()
-                        : null
-                    }
-                </View>
                 <View style={Styles.flex21}>
                     <Image
                         source = { WipLogo }
@@ -89,16 +71,6 @@ class MainPage extends Component {
                 />
             </View>
         )
-    }
-
-    __renderProfileImg() {
-        return <Image 
-            source={{uri: this.state.userProfile.pic}}
-            style={[
-                Styles.profileImg,
-                Styles.borderRadius10
-            ]}
-        />
     }
 }
 
