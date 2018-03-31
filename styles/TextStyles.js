e8e73fd601aebaa76d61d4f568a5491788e20e11
boat46450 <boat46450@hotmail.com>
2018-03-31 23:46:24 +0700

    EDIT refactor/ui add text size 12, 19

--- styles/TextStyles.js
+++ styles/TextStyles.js
@@ -1,11 +1,20 @@
 import { StyleSheet } from 'react-native'
 
 const style = StyleSheet.create({
+    size12: {
+        fontSize: 12
+    },
+    size14: {
+        fontSize: 14
+    },
     size16: {
         fontSize: 16
     },
     size18: {
         fontSize: 18
+    },
+    size19: {
+        fontSize: 19
     },
     bold: {
         fontWeight: 'bold'
