ac8222b36a491a41abd9f3744e76869f37ff2bb2
boat46450 <boat46450@hotmail.com>
2018-04-03 16:25:43 +0700

    EDIT refactor/ui add style for image in main page

--- styles/ImageStyle.js
+++ styles/ImageStyle.js
@@ -14,6 +14,17 @@ const styles = StyleSheet.create({
         width: height / 25,
         marginRight: 20,
     },
+    bannerMain: {
+        resizeMode: Image.resizeMode.cover
+    },
+    mainIcon: {
+        width: width * 0.25,
+        height: width * 0.25
+    },
+    mainIconS: {
+        width: width * 0.19,
+        height: width * 0.19
+    },
     problemIcon: {
         height: width * 0.2 - 20,
         width: width * 0.2 - 20,
@@ -23,6 +34,19 @@ const styles = StyleSheet.create({
         height : height * 0.25,
         resizeMode: Image.resizeMode.contain,
         right: width * -0.02
+    },
+    profileBanner: {
+        height: (height * 0.21) + (width * 0.3 / 2),
+        resizeMode: Image.resizeMode.cover,
+    },
+    profileImg: {
+        height: width * 0.3,
+        width: width * 0.3,
+        borderRadius: width * 0.3 / 2,
+        position: 'absolute',
+        zIndex: 100,
+        bottom: 0,
+        left: (width / 2) - (width * 0.3 / 2)
     }
 })
 
