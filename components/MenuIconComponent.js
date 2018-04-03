22f528e5ea2ef41576084b636b03cece5aa5feee
boat46450 <boat46450@hotmail.com>
2018-04-03 16:43:49 +0700

    ADD refactor/ui component for each menu

---
+++ components/MenuIconComponent.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { TouchableOpacity, Text, Image } from 'react-native'
+
+import LayoutStyles from '../styles/LayoutStyle'
+import ColorStyles from '../styles/ColorStyle'
+import TextStyles from '../styles/TextStyles'
+import ImageStyles from '../styles/ImageStyle'
+import ComponentStyles from '../styles/ComponentStyle'
+
+class MenuIcon extends Component {
+    render() {
+        return (
+            <TouchableOpacity
+                style={ this.__renderStyle() }
+                onPress={this.props.function}
+            >
+                <Image
+                    source={this.props.icon}
+                    style={ this.__renderImageStyle() }
+                />
+                <Text
+                    style={ this.__renderTextStyle() }
+                >
+                    {this.props.text}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+
+    __renderStyle() {
+        let styles = [
+            LayoutStyles.row,
+            LayoutStyles.alignCenter,
+            LayoutStyles.radius,
+            ColorStyles.bgWhite,
+            ColorStyles.borderBotBlack05
+        ]
+        let styles2 = []
+        if (this.props.two) {
+            styles2 = [
+                LayoutStyles.flex09,
+                LayoutStyles.row,
+                LayoutStyles.justifyAround
+            ]
+        }
+        else {
+            styles2 = [
+                LayoutStyles.flex045,
+                LayoutStyles.column,
+                LayoutStyles.justifyCenter
+            ]
+        }
+        return [...styles, ...styles2]
+    }
+
+    __renderImageStyle() {
+        let styles = []
+        if (this.props.two) {
+            styles = [
+                LayoutStyles.left95,
+                ImageStyles.mainIcon
+            ]
+        }
+        else {
+            styles = [
+                LayoutStyles.padB5,
+                ImageStyles.mainIconS
+            ]
+        }
+        return styles
+    }
+
+    __renderTextStyle() {
+        let styles = []
+        if (this.props.two) {
+            styles = [
+                LayoutStyles.right95,
+                TextStyles.size25
+            ]
+        }
+        else {
+            styles = [
+                LayoutStyles.padT5,
+                TextStyles.size16
+            ]
+        }
+        return styles
+    }
+}
+
+export default MenuIcon
