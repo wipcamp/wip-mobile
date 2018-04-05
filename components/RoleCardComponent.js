04fd9066f4183d622efe6c87e7b539c40dcc5560
boat46450 <boat46450@hotmail.com>
2018-04-05 10:14:05 +0700

    ADD refactor/ui role card for profile

---
+++ components/RoleCardComponent.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Icon } from 'native-base'
+
+import LayoutStyles from '../styles/LayoutStyle'
+import ColorStyles from '../styles/ColorStyle'
+import TextStyles from '../styles/TextStyles'
+
+class RoleCard extends Component {
+    render() {
+        return (
+            <View
+                style={LayoutStyles.row}
+                key={this.props.keys}
+            >
+                <View style={LayoutStyles.flex02} />
+                <View
+                    style={LayoutStyles.flex02}
+                >
+                    <Icon
+                        ios={this.props.ios}
+                        android={this.props.android}
+                        style={[
+                            TextStyles.right,
+                            ColorStyles.textOrange
+                        ]}
+                    />
+                </View>
+                <View
+                    style={LayoutStyles.flex06}
+                >
+                    <Text
+                        style={[
+                            LayoutStyles.padL06P,
+                            TextStyles.size16,
+                            TextStyles.kanit
+                        ]}
+                    >
+                        {this.props.role}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default RoleCard
